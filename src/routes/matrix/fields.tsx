e0524e72639fa654010abe0dfd8823baf816d5be
Roy QIU <karoyqiu@gmail.com>
2025-06-07 18:27:21 +0800

    添加 scroll area。

--- src/routes/matrix/fields.tsx
+++ src/routes/matrix/fields.tsx
@@ -6,6 +6,7 @@ import { useDebounceCallback } from 'usehooks-ts';
 import { FieldCollapsible } from '@/components/field-collapsible';
 import { InputBox } from '@/components/input-box';
 import { Button } from '@/components/ui/button';
+import { ScrollArea } from '@/components/ui/scroll-area';
 import { Separator } from '@/components/ui/separator';
 import { type ActionType, useMatrix, useMatrixDispatch } from '@/lib/matrix/context';
 
@@ -24,29 +25,31 @@ function RouteComponent() {
   };
 
   return (
-    <div className="flex flex-col gap-4 p-2">
-      {matrix.fields.map((field, index) => (
-        <Fragment key={field.id}>
-          <FieldCollapsible
-            {...{ index, total: matrix.fields.length, field, dispatch: dispatchAndSave }}
-          />
-          <Separator />
-        </Fragment>
-      ))}
-      <InputBox
-        title="Add Field"
-        description="Input the name of the new field:"
-        onInput={(name) => {
-          if (name) {
-            dispatchAndSave({ type: 'newField', field: { id: '', name, type: 'text' } });
-          }
-        }}
-      >
-        <Button>
-          <PlusIcon />
-          Add a new field
-        </Button>
-      </InputBox>
-    </div>
+    <ScrollArea className="h-screen">
+      <div className="flex flex-col gap-4 p-2">
+        {matrix.fields.map((field, index) => (
+          <Fragment key={field.id}>
+            <FieldCollapsible
+              {...{ index, total: matrix.fields.length, field, dispatch: dispatchAndSave }}
+            />
+            <Separator />
+          </Fragment>
+        ))}
+        <InputBox
+          title="Add Field"
+          description="Input the name of the new field:"
+          onInput={(name) => {
+            if (name) {
+              dispatchAndSave({ type: 'newField', field: { id: '', name, type: 'text' } });
+            }
+          }}
+        >
+          <Button>
+            <PlusIcon />
+            Add a new field
+          </Button>
+        </InputBox>
+      </div>
+    </ScrollArea>
   );
 }
